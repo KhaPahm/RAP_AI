4aa8baa6dd6c2ba366820f6c24e3263b403d580e
KhaPahm <khapham.work@gmail.com>
2024-01-02 13:42:45 +0700

    Create Project

---
+++ src/config/database.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mysq: {
+        host: "",
+        password: ""
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+
+const routerIndexPath = path.join(__dirname, "api", "routers", "index.routers.js");
+const router = require(routerIndexPath);
+
+//Config enviroment variable
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRECT_KEY,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(cookieParser());
+
+app.use(cors({
+    credentials: true,
+    origin: true
+}))
+
+app.use("/", router);
+
+app.use((req, res) => {
+    res.status(404).send({url: req.originalUrl + " not found!"});
+})
+
+app.listen(port, () => {
+    console.log(`Server is running in port: ${port}`);
+})
