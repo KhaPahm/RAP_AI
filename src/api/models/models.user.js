b58aba00cf1e8413988bf65cd51579445e3eab94
KhaPahm <khapham.work@gmail.com>
2024-01-06 20:56:57 +0700

    Kha

--- src/api/models/models.user.js
+++ src/api/models/models.user.js
@@ -2,7 +2,7 @@ const mysqlModel = require("./models.index");
 
 module.exports = {
     async GetAllUser() {
-        const strQuery = "SELECT * FROM user";
+        const strQuery = "SELECT * FROM User";
         const result = await mysqlModel.query(strQuery);
         console.log(result);
         return result;
--- src/config/database.config.js
+++ src/config/database.config.js
@@ -3,14 +3,14 @@ module.exports = {
         host: "localhost",
         user: "root",
         password: "root1234",
-        database: "rap_ai",
+        database: "RAP_AI",
         port: "3306"
     },
     mySqlRds: {
         host: "database-1.c7oqwugy2ays.ap-southeast-2.rds.amazonaws.com",
         user: "admin",
         password: "DbpF5t4ePBSbCvpfa6On",
-        database: "rap-ai",
+        database: "RAP_AI",
         port: "3306"
     }
 
