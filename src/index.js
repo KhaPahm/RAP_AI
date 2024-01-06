48532c11cea82ba0c472d61dc547b84afd5f43e0
KhaPahm <khapham.work@gmail.com>
2024-01-06 19:43:36 +0700

    edit env

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const router = require(routerIndexPath);
 //Config enviroment variable
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
